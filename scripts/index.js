6f0b14f211e0d6b726353386ba0f50effeceec6e
AcruxTech <acrux_tech@mail.ru>
2024-04-27 10:31:45 +0300

    add index.js

---
+++ scripts/index.js
@@ -0,0 +1,94 @@
+
+
+
+result = {
+    "surveys": [
+        {
+            "title": "мой опрос1",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос2",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос3",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос4",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос5",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос6",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос7",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос1",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос2",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос3",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос4",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос5",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос6",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        },
+        {
+            "title": "мой опрос7",
+            "topic": "какая то категория",
+            "show_result_after_passing": false,
+            questions: []
+        }
+    ]
+}
+
+const numSurveys = result.surveys.length;
+window.cnt = numSurveys;
