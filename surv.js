2db264eb48d3dab3be07f2d8b04bcbc913db8c74
Gleb <gleb.gleb.tula@mail.ru>
2024-04-28 03:13:53 +0300

    mini fix

--- surv.js
+++ surv.js
@@ -32,7 +32,7 @@ survey = {
 function render_name_surveys(survey) { // Добавление название опроса в заголовок
     
     let name_survey = survey["title"];
-
+    
     const text_surveyName = document.createElement('p'); // создание элемента p
     text_surveyName.classList.add('survey_name_p');
     text_surveyName.innerHTML = name_survey; // Добавление текста в элемент p
@@ -53,27 +53,32 @@ function render_question(survey) {
     for (let i = 0; i < count; i += 1) {
 
         let number_question = survey.questions[i]["title"];
+        let text_q = survey.questions[i]["text"];
         const div_number_question = document.createElement('div'); // создание элемента div в котором будет номер вопроса
         div_number_question.classList.add('number_of_question_div'+i); //добавление класса этому элемента
 
-        const text_number_question = document.createElement('p'); // создание элемента p
-
+        const text_number_question = document.createElement('p'); // создание элемента p с номером вопроса
+        const text_question = document.createElement('p'); // создание элемента p где будет сам вопрос
         text_number_question.innerHTML = number_question;
-        text_number_question.classList.add('number_question_p');
+        text_question.innerHTML = text_q; // Тут находится сам вопрос
 
-        div_number_question.appendChild(text_number_question);
+        text_number_question.classList.add('number_question_p');
+        text_question.classList.add('text_question_p');
+
+        div_number_question.appendChild(text_number_question); // добавление в (div  div_number_question) двух элементов, которые показывают номер вопроса и текст вопроса
+        div_number_question.appendChild(text_question); // добавление в (div  div_number_question) двух элементов, которые показывают номер вопроса и текст вопроса
 
         const container = document.querySelector('.list_task'); // получение элемента container, в него будут добавлены  div_number_question
-        container.appendChild(div_number_question); // добавить элемент div_number_question в new-survey
 
+        container.appendChild(div_number_question); // добавить элемент div_number_question (В котором находится и вопрос и текст вопроса) в new-survey
         // и того мы добавили container div в котором содержится что-то типо "Вопрос 1:"
         // далее с помощью цикла я переберу и выведу нужные мне варианты ответа на вопрос
 
         let count_answer = survey.questions[i]['answers'].length;
         for (let j = 0; j < count_answer; j += 1) { // В этом цикле буду перебирать все возможные варианты ответов
-            let answ = survey.questions[i]['answers'][j];
+            let answ = survey.questions[i]['answers'][j]; // тут находится сам текст вопроса
             const text_answ = document.createElement('p');
-            text_answ.innerHTML = answ;
+            text_answ.innerHTML = answ; // помещаем  один из ответов на вопрос в файл
             text_answ.classList.add('text_answ_p');
 
             const container = document.querySelector('.number_of_question_div'+i);
