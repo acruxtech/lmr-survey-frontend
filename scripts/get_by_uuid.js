60159f26a798cdfbea454cd291c373f02e6f269d
AcruxTech <acrux_tech@mail.ru>
2024-04-28 11:26:12 +0300

    fix redirect

--- scripts/get_by_uuid.js
+++ scripts/get_by_uuid.js
@@ -3,13 +3,9 @@
 let btn = document.getElementById("get-by-uuid");
 
 // On click event
-btn.addEventListener("click", get_by_uuid);
-
-function get_by_uuid() {
-
+btn.addEventListener("click", e => {
     div = document.querySelector("#get-by-uuid")
     div.style.display = "none";
-
 
     header_buttons = document.querySelector(".header-buttons");
     form = document.createElement("form");
@@ -20,12 +16,17 @@ function get_by_uuid() {
     input.setAttribute("name", "uuid")
     button = document.createElement("button");
     button.innerHTML = "Поиск"
-    button.setAttribute("onclick", "get_survey_by_uuid()")
+
+    button.addEventListener("click", e => {
+        e.preventDefault();
+        window.location.href = "pass.html?uuid=" + input.value;
+    })
+
     form.appendChild(input);
     form.appendChild(button);
     header_buttons.appendChild(form);
+});
 
-}
 
 // Out click event
 uuid_form = document.getElementsByClassName("uuid-form")
@@ -33,37 +34,11 @@ if (uuid_form) {
     main = document.getElementsByTagName("main")[0];
     main.addEventListener("click", out_of_uuid);
     function out_of_uuid(event) {
-      // event.preventDefault()
+      event.preventDefault()
       div = document.querySelector(".uuid-form");
       div.remove()
       
       div = document.querySelector("#get-by-uuid")
       div.style.display = "block";
-      
   }
 }
-
-
-
-
-
-// code for fetch GET data and redirect
-
-function get_survey_by_uuid() {
-    window.location.href = 'newPage.html';
-    // let params = new URLSearchParams(location.search);
-    // let uuid = params.get('uuid')
-
-    // // get current URL 
-    // let url = window.location.href.split('/');
-    // url[url.length - 1] = "pass.html?uuid=" + uuid;
-    // url = url.join('/')
-    // console.log(url)
-
-    // // redirect
-    // // window.location = "file:///D:/lmr-survey-frontend/pass.html?uuid=6f34d13a-a510-4e15-9d9c-705424073f38";
-    // window.location.href = url
-    // console.log(window.location.href)
-    
-}
-
